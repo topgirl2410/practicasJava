ce60ea8f10b9e2ceb619ad813744b95d0a97e226
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-12 18:04:10 +0100

    ejercicio 2 math.sqrt

---
+++ politecnicoJava/src/claseMath/Ejercicio2.java
@@ -0,0 +1,16 @@
+package claseMath;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Raiz de 2: " + Math.sqrt(2));
+		System.out.println("Raiz de 8: " + Math.sqrt(8));
+		System.out.println("Raiz de 9: " + Math.sqrt(9));
+		System.out.println("Raiz de 27: " + Math.sqrt(27));
+		System.out.println("Raiz de 28: " + Math.sqrt(28));
+		System.out.println("Raiz de 55: " + Math.sqrt(55));
+		System.out.println("Raiz de 121: " + Math.sqrt(121));
+	}
+
+}
