4fb8e48284b275687abb0960d39c133d0a9b2e2c
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-13 00:56:41 +0100

    variable con double

---
+++ politecnicoJava/src/Variables/Ejercicio5.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double resultado = (8 * 0.2) + 2 / 1.8 - (2 - (1 * 8) - 8 * 2);
+		
+		System.out.println("El resultado es: " + resultado);
+	}
+
+}
