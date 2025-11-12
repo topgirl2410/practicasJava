00a9bc6ecbb6ba43a10a93808186d4cec2abcd85
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-12 17:57:54 +0100

    Mi primer commit

---
+++ politecnicoJava/src/claseMath/Ejercicio1.java
@@ -0,0 +1,11 @@
+package claseMath;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		int base = 8;
+		int exponente = 3;
+		int potencia = (int) Math.pow(base, exponente);
+
+		System.out.println("La potencia es: " + potencia);
+	}
+}
