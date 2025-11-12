0b395081e022f537e92f3dc968fe050dd5715266
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-13 00:48:10 +0100

    variable auxiliar

---
+++ politecnicoJava/src/Variables/Ejercicio3.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1 = 8;
+		int numero2 = 2;
+		
+		int aux;
+		aux = numero1;
+		 numero1 = numero2;
+		 numero2 = aux;
+		System.out.println("El nuevo valor es: " + numero1 );
+		System.out.println("El nuevo valor es: " + numero2 );
+	}
+
+}
