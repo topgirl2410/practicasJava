11e76130cf753060b5e6fd69e85b022d32b16ddf
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-13 00:35:18 +0100

    promedios

---
+++ politecnicoJava/src/Variables/Ejercicio1.java
@@ -0,0 +1,19 @@
+package Variables;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Maria";
+		double num1 = 5.2;
+		double num2 = 3.5;
+		double num3 = 6.7;
+		double num4 = 7.8;
+		double num5 = 9.8;
+
+		double promedio = (num1 + num2 + num3 + num4 + num5) / 5;
+
+		System.out.println("El promedio de " + nombre + " es: " + promedio);
+	}
+
+}
