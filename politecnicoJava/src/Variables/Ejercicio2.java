8a3ec5d2c89ef51324ec1eeb0e9272a8c3dbd785
topgirl2410 <maria.topgirl82@gmail.com>
2025-11-13 00:41:12 +0100

    variables

---
+++ politecnicoJava/src/Variables/Ejercicio2.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 9;
+		int num2 = 7;
+		
+		int suma = num1 + num2;
+		System.out.println("Suma: " + suma);
+		
+		int resta = num1 - num2;
+		System.out.println("Resta: " + resta);
+		
+		int multi = num1 * num2;
+		System.out.println("Multi: " + multi);
+		
+		int division = num1 / num2;
+		System.out.println("Division: " + division);
+	}
+
+}
